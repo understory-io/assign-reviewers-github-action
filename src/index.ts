18311f7108282561e3a2dc3196e346d5fc303bea
Bjorn <bjoern@understory.io>
2025-02-23 16:05:53 +0100

    feat: initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,45 @@
-/**
- * The entrypoint for the action. This file simply imports and runs the action's
- * main logic.
- */
-import { run } from './main.js'
+import * as core from "@actions/core";
+import { context } from "@actions/github";
+import { assignReviewers } from "./assignReviewers";
 
-/* istanbul ignore next */
-run()
+async function run() {
+  try {
+    const target = context.payload.pull_request;
+    if (target === undefined) {
+      throw new Error("Can't get payload. Check you trigger event");
+    }
+    const {
+      number,
+      user: { login: userLogin },
+    } = target;
+
+    const token = core.getInput("token", { required: true });
+
+    const authors = await assignReviewers({
+      number,
+      token,
+      debug: core.debug,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      userLogin,
+    });
+
+    if (authors.length === 0) {
+      core.info(
+        `No reviewers have been assigned to the pull request: #${number}`
+      );
+    } else {
+      core.info(
+        `${authors
+          .map((a) => `@${a}`)
+          .join(", ")} has been assigned to the pull request: #${number}`
+      );
+    }
+  } catch (error: any) {
+    core.debug("context.payload: " + JSON.stringify(context.payload));
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
